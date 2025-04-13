9ea8d6cdbc32f9a615aee6dadb4ef7e119fdee0b
metallicpearl <borismcbobble@hotmail.com>
2025-04-13 18:51:54 +0100

    Updates

--- src/components/reset/reset.tsx
+++
@@ -1,9 +0,0 @@
-import { useChickenContext } from '../../chicken-context';
-import classes from './reset.module.scss';
-
-export const Reset = () => {
-    const {setChickenLocations, setEggLocations, setCount} = useChickenContext();
-    return (
-        <div className={classes.Reset} onClick={()=>{setChickenLocations([]); setEggLocations([]); setCount(0);}}>RESET</div>
-    )
-}
